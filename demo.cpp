3525c23dd0811f9e55b721ae12e11649a68c1836
kert <kaidokert@gmail.com>
2020-09-23 21:57:27 -0700

    Run

--- demo.cpp
+++ demo.cpp
@@ -2,6 +2,10 @@
 
 int main() {
     YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
-    printf("Yay!\n");
+    printf("Yay %d!\n",primes.IsSequence());
+    for(auto el = primes.begin(); el != primes.end(); ++el) {
+        int num = el->as<int>();
+        printf("%d\n",num);
+    }
     return 0;
 }
