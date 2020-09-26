3b45cddbaa24e41e7eded183faeef417cbb045b6
kert <kaidokert@gmail.com>
2020-09-26 10:30:37 -0700

    Make mocks work

---
+++ demo_interface.h
@@ -0,0 +1,12 @@
+
+#include <string>
+
+class Interface
+{
+public:
+  virtual ~Interface() = default;
+  virtual bool foo(int, std::string& s) = 0;
+  virtual bool bar(int) = 0;
+  virtual bool bar(std::string) = 0;
+};
+
