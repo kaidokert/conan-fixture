56e70b21b7aa1c594ff6325aa44373783459c91f
kert <kaidokert@gmail.com>
2020-09-26 10:53:05 -0700

    Format and fix

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,11 @@
 #include "yaml-cpp/yaml.h"
 
 int main() {
-    YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
-    printf("Yay %d!\n",primes.IsSequence());
-    for(auto el = primes.begin(); el != primes.end(); ++el) {
-        int num = el->as<int>();
-        printf("%d\n",num);
-    }
-    return 0;
+  YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
+  printf("Yay %d!\n", primes.IsSequence());
+  for (auto el = primes.begin(); el != primes.end(); ++el) {
+    int num = el->as<int>();
+    printf("%d\n", num);
+  }
+  return 0;
 }
--- demo_tests.cpp
+++ demo_tests.cpp
@@ -1,20 +1,21 @@
 #define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
 #include <vector>
 
-SCENARIO( "This works", "[foo]" ) {
-    GIVEN("something") {
-        std::vector<int> v(5);
-        REQUIRE( v.size() == 5 );
+#include "catch2/catch.hpp"
 
-        WHEN( "the size is increased" ) {
-            v.resize( 10 );
+SCENARIO("This works", "[foo]") {
+  GIVEN("something") {
+    std::vector<int> v(5);
+    REQUIRE(v.size() == 5);
 
-            THEN( "the size and capacity change" ) {
-                REQUIRE( v.size() == 10 );
-                REQUIRE( v.capacity() >= 10 );
-                //REQUIRE( 1 == 2 );
-            }
-        }
+    WHEN("the size is increased") {
+      v.resize(10);
+
+      THEN("the size and capacity change") {
+        REQUIRE(v.size() == 10);
+        REQUIRE(v.capacity() >= 10);
+        // REQUIRE( 1 == 2 );
+      }
     }
+  }
 }
