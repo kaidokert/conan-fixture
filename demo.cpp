ef6195eb8574e660f471d05329cfdba7feb57e01
kert <kaidokert@gmail.com>
2020-09-26 23:24:45 -0700

    Fix linter

--- demo.cpp
+++ demo.cpp
@@ -4,7 +4,7 @@ auto main() -> int { // NOLINT(bugprone-exception-escape)
   YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
   printf("Yay %d!\n", static_cast<int>(primes.IsSequence()));
   for (auto el = primes.begin(); el != primes.end(); ++el) {
-    int num = el->as<int>();
+    auto num = el->as<int>();
     printf("%d\n", num);
   }
   return 0;
